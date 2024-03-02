bff9cdc6e5ebb87b9bd3911e763f23ebf3173644
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-03-02 17:43:45 +0100

    added boilerplate window code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+use acerola_jam_0::*;
+
 fn main() {
-    println!("Hello, world!");
+    let window_plugin = WindowPlugin {
+        primary_window: Some(Window {
+            mode: bevy::window::WindowMode::BorderlessFullscreen,
+            ..Default::default()
+        }),
+        ..default()
+    };
+
+    App::new()
+        .add_plugins((
+            DefaultPlugins
+                .set(window_plugin),
+            SetupGamePlugin
+        ))
+        .run();
 }
